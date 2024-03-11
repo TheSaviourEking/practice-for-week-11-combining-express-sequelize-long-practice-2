f89cefd7893fdbf00ef2a0900967feb8ecca5854
TheSaviourEking <saviour.eking@outlook.com>
2024-03-11 04:18:31 +0100

    chore: implement Intermediate Phase 5: Join Table & Associations

---
+++ server/db/migrations/20240311030751-create-insect-tree.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('InsectTrees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      insectId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Insects'
+        },
+        onDelete: 'CASCADE'
+      },
+      treeId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Trees'
+        },
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('InsectTrees');
+  }
+};
